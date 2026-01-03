b2f30f72cb007b7058912af9ed15df9032d47fd1
dezsoseres <seresd5@gmail.com>
2026-01-03 21:16:08 +0100

    added gitignore, unified tomi.bat

--- tomi.bat
+++ tomi.bat
@@ -1,9 +1,15 @@
 del *.rar
-del *.obj
-del *.bak
-del *.err
-rem del *.exe
-C:\progra~1\seresd\winrar\rar a -r vlang_mastermind08
+del *.exe
+for /f "tokens=1-3 delims=. " %%a in ('date /t') do (
+    set yyyy=%%a
+    set mm=%%b
+    set dd=%%c
+)
+set DATE_YYYYMMDD=%yyyy%%mm%%dd%
+echo %DATE_YYYYMMDD%
+c:\"program files"\seresd\winrar\rar a -r -s videobrowser%DATE_YYYYMMDD%
+
+
 
 
 
